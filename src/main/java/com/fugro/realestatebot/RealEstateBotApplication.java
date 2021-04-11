542299be3341c0cdabda142341ca672b4cc9ff6e
fugrusha <andrew.golowko@gmail.com>
2021-04-11 18:56:23 +0300

    initial commit

---
+++ src/main/java/com/fugro/realestatebot/RealEstateBotApplication.java
@@ -0,0 +1,13 @@
+package com.fugro.realestatebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateBotApplication.class, args);
+    }
+
+}
